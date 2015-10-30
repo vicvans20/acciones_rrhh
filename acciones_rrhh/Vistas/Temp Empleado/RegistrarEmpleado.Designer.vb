ba43e7926612f4fe0ea8d346ae7cb3130ac2687b
vicvans20 <vicvans20@gmail.com>
2015-10-30 15:29:40 -0600

    Add data grid view to look at the records in empleados form.

--- acciones_rrhh/Vistas/Temp Empleado/RegistrarEmpleado.Designer.vb
+++ acciones_rrhh/Vistas/Temp Empleado/RegistrarEmpleado.Designer.vb
@@ -40,7 +40,9 @@
             Me.Label6 = New System.Windows.Forms.Label()
             Me.Label7 = New System.Windows.Forms.Label()
             Me.Label8 = New System.Windows.Forms.Label()
+            Me.DataGridView1 = New System.Windows.Forms.DataGridView()
             CType(Me.nSalario, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             '
             'bGuardar
@@ -54,56 +56,56 @@
             '
             'tbNombre
             '
-            Me.tbNombre.Location = New System.Drawing.Point(291, 12)
+            Me.tbNombre.Location = New System.Drawing.Point(95, 14)
             Me.tbNombre.Name = "tbNombre"
             Me.tbNombre.Size = New System.Drawing.Size(100, 20)
             Me.tbNombre.TabIndex = 1
             '
             'tbApellidos
             '
-            Me.tbApellidos.Location = New System.Drawing.Point(291, 57)
+            Me.tbApellidos.Location = New System.Drawing.Point(95, 59)
             Me.tbApellidos.Name = "tbApellidos"
             Me.tbApellidos.Size = New System.Drawing.Size(100, 20)
             Me.tbApellidos.TabIndex = 2
             '
             'tbCedula
             '
-            Me.tbCedula.Location = New System.Drawing.Point(291, 106)
+            Me.tbCedula.Location = New System.Drawing.Point(95, 108)
             Me.tbCedula.Name = "tbCedula"
             Me.tbCedula.Size = New System.Drawing.Size(100, 20)
             Me.tbCedula.TabIndex = 3
             '
             'tbTelefono
             '
-            Me.tbTelefono.Location = New System.Drawing.Point(291, 155)
+            Me.tbTelefono.Location = New System.Drawing.Point(95, 157)
             Me.tbTelefono.Name = "tbTelefono"
             Me.tbTelefono.Size = New System.Drawing.Size(100, 20)
             Me.tbTelefono.TabIndex = 4
             '
             'tbDepartamento
             '
-            Me.tbDepartamento.Location = New System.Drawing.Point(482, 11)
+            Me.tbDepartamento.Location = New System.Drawing.Point(286, 13)
             Me.tbDepartamento.Name = "tbDepartamento"
             Me.tbDepartamento.Size = New System.Drawing.Size(100, 20)
             Me.tbDepartamento.TabIndex = 5
             '
             'tbCargo
             '
-            Me.tbCargo.Location = New System.Drawing.Point(482, 54)
+            Me.tbCargo.Location = New System.Drawing.Point(286, 56)
             Me.tbCargo.Name = "tbCargo"
             Me.tbCargo.Size = New System.Drawing.Size(100, 20)
             Me.tbCargo.TabIndex = 6
             '
             'tbInss
             '
-            Me.tbInss.Location = New System.Drawing.Point(482, 148)
+            Me.tbInss.Location = New System.Drawing.Point(286, 105)
             Me.tbInss.Name = "tbInss"
             Me.tbInss.Size = New System.Drawing.Size(100, 20)
             Me.tbInss.TabIndex = 8
             '
             'nSalario
             '
-            Me.nSalario.Location = New System.Drawing.Point(482, 104)
+            Me.nSalario.Location = New System.Drawing.Point(286, 164)
             Me.nSalario.Name = "nSalario"
             Me.nSalario.Size = New System.Drawing.Size(100, 20)
             Me.nSalario.TabIndex = 9
@@ -111,7 +113,7 @@
             'Label1
             '
             Me.Label1.AutoSize = True
-            Me.Label1.Location = New System.Drawing.Point(225, 18)
+            Me.Label1.Location = New System.Drawing.Point(29, 20)
             Me.Label1.Name = "Label1"
             Me.Label1.Size = New System.Drawing.Size(44, 13)
             Me.Label1.TabIndex = 10
@@ -120,7 +122,7 @@
             'Label2
             '
             Me.Label2.AutoSize = True
-            Me.Label2.Location = New System.Drawing.Point(225, 60)
+            Me.Label2.Location = New System.Drawing.Point(29, 62)
             Me.Label2.Name = "Label2"
             Me.Label2.Size = New System.Drawing.Size(44, 13)
             Me.Label2.TabIndex = 11
@@ -129,7 +131,7 @@
             'Label3
             '
             Me.Label3.AutoSize = True
-            Me.Label3.Location = New System.Drawing.Point(225, 106)
+            Me.Label3.Location = New System.Drawing.Point(29, 108)
             Me.Label3.Name = "Label3"
             Me.Label3.Size = New System.Drawing.Size(40, 13)
             Me.Label3.TabIndex = 12
@@ -138,7 +140,7 @@
             'Label4
             '
             Me.Label4.AutoSize = True
-            Me.Label4.Location = New System.Drawing.Point(225, 162)
+            Me.Label4.Location = New System.Drawing.Point(29, 164)
             Me.Label4.Name = "Label4"
             Me.Label4.Size = New System.Drawing.Size(49, 13)
             Me.Label4.TabIndex = 13
@@ -147,7 +149,7 @@
             'Label5
             '
             Me.Label5.AutoSize = True
-            Me.Label5.Location = New System.Drawing.Point(402, 18)
+            Me.Label5.Location = New System.Drawing.Point(206, 20)
             Me.Label5.Name = "Label5"
             Me.Label5.Size = New System.Drawing.Size(74, 13)
             Me.Label5.TabIndex = 14
@@ -156,7 +158,7 @@
             'Label6
             '
             Me.Label6.AutoSize = True
-            Me.Label6.Location = New System.Drawing.Point(406, 60)
+            Me.Label6.Location = New System.Drawing.Point(210, 62)
             Me.Label6.Name = "Label6"
             Me.Label6.Size = New System.Drawing.Size(35, 13)
             Me.Label6.TabIndex = 15
@@ -165,7 +167,7 @@
             'Label7
             '
             Me.Label7.AutoSize = True
-            Me.Label7.Location = New System.Drawing.Point(406, 106)
+            Me.Label7.Location = New System.Drawing.Point(210, 166)
             Me.Label7.Name = "Label7"
             Me.Label7.Size = New System.Drawing.Size(39, 13)
             Me.Label7.TabIndex = 16
@@ -174,17 +176,29 @@
             'Label8
             '
             Me.Label8.AutoSize = True
-            Me.Label8.Location = New System.Drawing.Point(402, 155)
+            Me.Label8.Location = New System.Drawing.Point(206, 112)
             Me.Label8.Name = "Label8"
             Me.Label8.Size = New System.Drawing.Size(62, 13)
             Me.Label8.TabIndex = 17
             Me.Label8.Text = "Codigo Inss"
+            '
+            'DataGridView1
+            '
+            Me.DataGridView1.AllowUserToAddRows = False
+            Me.DataGridView1.AllowUserToDeleteRows = False
+            Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+            Me.DataGridView1.Location = New System.Drawing.Point(446, 13)
+            Me.DataGridView1.Name = "DataGridView1"
+            Me.DataGridView1.ReadOnly = True
+            Me.DataGridView1.Size = New System.Drawing.Size(450, 171)
+            Me.DataGridView1.TabIndex = 18
             '
             'RegistrarEmpleado
             '
             Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(908, 262)
+            Me.Controls.Add(Me.DataGridView1)
             Me.Controls.Add(Me.Label8)
             Me.Controls.Add(Me.Label7)
             Me.Controls.Add(Me.Label6)
@@ -205,6 +219,7 @@
             Me.Name = "RegistrarEmpleado"
             Me.Text = "RegistrarEmpleado"
             CType(Me.nSalario, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
             Me.PerformLayout()
 
@@ -226,5 +241,6 @@
         Friend WithEvents Label6 As System.Windows.Forms.Label
         Friend WithEvents Label7 As System.Windows.Forms.Label
         Friend WithEvents Label8 As System.Windows.Forms.Label
+        Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
     End Class
 End Namespace
--- acciones_rrhh/Vistas/Temp Empleado/RegistrarEmpleado.vb
+++ acciones_rrhh/Vistas/Temp Empleado/RegistrarEmpleado.vb
@@ -1,6 +1,14 @@
 ﻿Namespace Vistas
     Public Class RegistrarEmpleado
-
+        Private Sub UpdateDataGrid(ByVal context As rrhhEntities, ByVal dg As DataGridView)
+            Dim empList = (From emp In context.empleados Select emp.cedula, emp.primer_nombre, emp.apellidos, emp.salario).ToList
+            dg.DataSource = empList
+        End Sub
+        Private Sub RegistrarEmpleado_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            Using context As New rrhhEntities
+                UpdateDataGrid(context, Me.DataGridView1)
+            End Using
+        End Sub
         Private Sub bGuardar_Click(sender As Object, e As EventArgs) Handles bGuardar.Click
             Using context As New rrhhEntities
                 Dim emp As New empleado
@@ -15,8 +23,8 @@
                 emp.fecha_contratacion = Date.Today
                 context.empleados.Add(emp)
                 context.SaveChanges()
+                UpdateDataGrid(context, Me.DataGridView1)
             End Using
-
         End Sub
     End Class
 End Namespace
