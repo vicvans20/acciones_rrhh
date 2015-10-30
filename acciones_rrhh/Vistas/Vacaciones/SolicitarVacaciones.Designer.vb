3f02e91be5ee5f1757ce2feb7968256e584cd15a
vicvans20 <vicvans20@gmail.com>
2015-10-30 16:33:54 -0600

    Add solicitar vacaciones form.

--- acciones_rrhh/Vistas/Vacaciones/SolicitarVacaciones.Designer.vb
+++ acciones_rrhh/Vistas/Vacaciones/SolicitarVacaciones.Designer.vb
@@ -23,17 +23,85 @@
         'No lo modifique con el editor de código.
         <System.Diagnostics.DebuggerStepThrough()> _
         Private Sub InitializeComponent()
+            Me.cbEmpleados = New System.Windows.Forms.ComboBox()
+            Me.bSend = New System.Windows.Forms.Button()
+            Me.dateInicio = New System.Windows.Forms.DateTimePicker()
+            Me.dateFin = New System.Windows.Forms.DateTimePicker()
+            Me.Label1 = New System.Windows.Forms.Label()
+            Me.Label2 = New System.Windows.Forms.Label()
             Me.SuspendLayout()
+            '
+            'cbEmpleados
+            '
+            Me.cbEmpleados.FormattingEnabled = True
+            Me.cbEmpleados.Location = New System.Drawing.Point(175, 27)
+            Me.cbEmpleados.Name = "cbEmpleados"
+            Me.cbEmpleados.Size = New System.Drawing.Size(474, 21)
+            Me.cbEmpleados.TabIndex = 0
+            '
+            'bSend
+            '
+            Me.bSend.Location = New System.Drawing.Point(367, 209)
+            Me.bSend.Name = "bSend"
+            Me.bSend.Size = New System.Drawing.Size(75, 23)
+            Me.bSend.TabIndex = 1
+            Me.bSend.Text = "Solicitar"
+            Me.bSend.UseVisualStyleBackColor = True
+            '
+            'dateInicio
+            '
+            Me.dateInicio.Location = New System.Drawing.Point(300, 78)
+            Me.dateInicio.Name = "dateInicio"
+            Me.dateInicio.Size = New System.Drawing.Size(220, 20)
+            Me.dateInicio.TabIndex = 2
+            '
+            'dateFin
+            '
+            Me.dateFin.Location = New System.Drawing.Point(300, 117)
+            Me.dateFin.Name = "dateFin"
+            Me.dateFin.Size = New System.Drawing.Size(220, 20)
+            Me.dateFin.TabIndex = 3
+            '
+            'Label1
+            '
+            Me.Label1.AutoSize = True
+            Me.Label1.Location = New System.Drawing.Point(386, 55)
+            Me.Label1.Name = "Label1"
+            Me.Label1.Size = New System.Drawing.Size(65, 13)
+            Me.Label1.TabIndex = 4
+            Me.Label1.Text = "Fecha Inicio"
+            '
+            'Label2
+            '
+            Me.Label2.AutoSize = True
+            Me.Label2.Location = New System.Drawing.Point(386, 101)
+            Me.Label2.Name = "Label2"
+            Me.Label2.Size = New System.Drawing.Size(54, 13)
+            Me.Label2.TabIndex = 5
+            Me.Label2.Text = "Fecha Fin"
             '
             'Solicitar_Vacaciones
             '
             Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(782, 262)
+            Me.ClientSize = New System.Drawing.Size(838, 262)
+            Me.Controls.Add(Me.Label2)
+            Me.Controls.Add(Me.Label1)
+            Me.Controls.Add(Me.dateFin)
+            Me.Controls.Add(Me.dateInicio)
+            Me.Controls.Add(Me.bSend)
+            Me.Controls.Add(Me.cbEmpleados)
             Me.Name = "Solicitar_Vacaciones"
             Me.Text = "Solicitar Vacaciones"
             Me.ResumeLayout(False)
+            Me.PerformLayout()
 
         End Sub
+        Friend WithEvents cbEmpleados As System.Windows.Forms.ComboBox
+        Friend WithEvents bSend As System.Windows.Forms.Button
+        Friend WithEvents dateInicio As System.Windows.Forms.DateTimePicker
+        Friend WithEvents dateFin As System.Windows.Forms.DateTimePicker
+        Friend WithEvents Label1 As System.Windows.Forms.Label
+        Friend WithEvents Label2 As System.Windows.Forms.Label
     End Class
 End Namespace
--- acciones_rrhh/Vistas/Vacaciones/SolicitarVacaciones.vb
+++ acciones_rrhh/Vistas/Vacaciones/SolicitarVacaciones.vb
@@ -1,5 +1,30 @@
 ﻿Namespace Vistas
     Public Class Solicitar_Vacaciones
 
+        Private Sub Solicitar_Vacaciones_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            Using context As New rrhhEntities
+                Dim empList = (From emp In context.empleados Select emp.primer_nombre, emp.apellidos, emp.id).ToList
+                cbEmpleados.DataSource = empList
+                cbEmpleados.DisplayMember = "primer_nombre"
+                cbEmpleados.ValueMember = "id"
+            End Using
+        End Sub
+
+        Private Sub bSend_Click(sender As Object, e As EventArgs) Handles bSend.Click
+            Using context As New rrhhEntities
+                Dim vac As New vacacione
+                vac.fecha_inicio = dateInicio.Value
+                vac.fecha_fin = dateFin.Value
+                vac.id_emp = cbEmpleados.SelectedValue
+                vac.fecha_creacion = Today
+                context.vacaciones.Add(vac)
+                Try
+                    context.SaveChanges()
+                    MessageBox.Show("Solicitud guardada")
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+            End Using
+        End Sub
     End Class
 End Namespace
